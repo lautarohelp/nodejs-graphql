e4060a3ccf03ebb09f4a29613c0e8c7c8782104c
lautarohelp <lautarotoledo391@gmail.com>
2024-01-18 15:25:23 -0600

    Mutations and Inputs

--- src/graphql/product.resolvers.js
+++ src/graphql/product.resolvers.js
@@ -12,4 +12,9 @@ const getProducts = async () => {
   return product;
 }
 
-module.exports = { getProduct, getProducts }
+const addProduct = async (_, { dto }) => {
+  const newProduct = await service.create(dto);
+  return newProduct;
+}
+
+module.exports = { getProduct, getProducts, addProduct }
